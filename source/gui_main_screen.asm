77edd0eb30ff12251b5b726bf31985794393474d
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-02-23 17:29:05 +0100

    Update gui_main_screen.asm

--- source/gui_main_screen.asm
+++ source/gui_main_screen.asm
@@ -13,7 +13,12 @@ main_screen:
  PRINT 2, 2, str_up, 'Welcome in BleskOS'
  PRINT 4, 2, str_you_can, 'You can open programs with keyboard:'
  PRINT 6, 2, str_te, '[a] Text editor'
+ PRINT 22, 2, str_down, 'You can shutdown computer with press power button'
 
  halt:
   WAIT_FOR_KEYBOARD
+
+  IF al, 'a', text_editor
+   jmp text_editor
+  ENDIF text_editor
  jmp halt
