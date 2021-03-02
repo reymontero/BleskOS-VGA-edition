d6b8e5f75385edac0450d2704f049717e0a681ba
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-03-02 16:24:54 +0100

    Update driver_keyboard.asm

--- source/driver_keyboard.asm
+++ source/driver_keyboard.asm
@@ -13,12 +13,21 @@
 %define KEY_F3 0x3D
 %define KEY_F4 0x3E
 
+%define KEY_A 0x1E
+%define KEY_B 0x30
+%define KEY_C 0x2E
+%define KEY_D 0x20
+%define KEY_E 0x12
+%define KEY_F 0x21
+%define KEY_G 0x22
+%define KEY_H 0x23
+
 %macro WAIT_FOR_KEYBOARD 0
  mov ah, 0x00
  int 16h
 %endmacro
 
-%macro READ_KEYBOARD 0
+%macro READ_FROM_KEYBOARD 0
  mov ah, 0x01
  int 16h
 %endmacro
