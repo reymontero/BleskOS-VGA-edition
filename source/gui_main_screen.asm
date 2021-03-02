c4a56a8d2a619d27186ed7895f6dc82b5c1f6a5f
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-03-02 16:24:31 +0100

    Update gui_main_screen.asm

--- source/gui_main_screen.asm
+++ source/gui_main_screen.asm
@@ -11,30 +11,27 @@ main_screen:
  DRAW_COLUMN 0, 79, 25, 0x40
 
  PRINT 2, 2, str_up, 'Welcome in BleskOS'
- PRINT 4, 2, str_you_can, 'You can open programs with keyboard:'
+ PRINT 4, 2, str_you_can, 'You can open programs with keyboard and go back with esc:'
  PRINT 6, 2, str_te, '[a] Text editor'
  PRINT 8, 2, str_ge, '[b] Graphic editor'
+ PRINT 10, 2, str_se, '[c] Sound editor'
  PRINT 20, 2, str_dz, '[F1] Developer zone'
  PRINT 22, 2, str_down, 'You can shutdown computer by pressing power button'
 
  .halt:
   WAIT_FOR_KEYBOARD
 
-  IF al, 'a', key_a
+  IF ah, KEY_A, key_a
    jmp text_editor
   ENDIF key_a
 
-  IF al, 'A', key_A
-   jmp text_editor
-  ENDIF key_A
-
-  IF al, 'b', key_b
+  IF ah, KEY_B, key_b
    jmp graphic_editor
   ENDIF key_b
 
-  IF al, 'B', key_B
-   jmp graphic_editor
-  ENDIF key_B
+  IF ah, KEY_C, key_c
+   jmp sound_editor
+  ENDIF key_c
 
   IF ah, KEY_F1, key_f1
    jmp developer_zone
@@ -62,15 +59,15 @@ developer_zone:
    jmp main_screen
   ENDIF key_esc
 
-  IF al, 'a', key_a
+  IF al, KEY_A, key_a
    jmp dz_vga
   ENDIF key_a
 
-  IF al, 'b', key_b
+  IF al, KEY_B, key_b
    jmp dz_pc_speaker
   ENDIF key_b
 
-  IF al, 'c', key_c
+  IF al, KEY_C, key_c
    jmp dz_drives
   ENDIF key_c
  jmp .halt
